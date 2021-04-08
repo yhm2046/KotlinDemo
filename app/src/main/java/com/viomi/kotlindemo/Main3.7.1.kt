930070acb54ae78724bc6d621a53015b533f57be
yhm2046 <yhm2046@gmail.com>
2021-04-08 17:25:25 +0800

    run,apply function

--- app/src/main/java/com/viomi/kotlindemo/Main3.7.1.kt
+++ app/src/main/java/com/viomi/kotlindemo/Main3.7.1.kt
@@ -1,6 +1,27 @@
 package com.viomi.kotlindemo
 
 fun main() {
+    //吃完所有水果并打印,demo4
+    println("--------------demo4---------------------")
+    val list4= listOf("apple","banana","pear")
+    val result4=StringBuffer().apply{
+        append("eating...\n")
+        for (fruit in list4)
+            append(fruit).append("\n")
+        append("ate finish")
+    }
+    println(result4.toString())
+    //吃完所有水果并打印,demo3
+    println("--------------demo3---------------------")
+    val list3= listOf("apple","banana","pear")
+    val result3=StringBuffer().run{
+        append("eating...\n")
+        for (fruit in list3)
+            append(fruit).append("\n")
+        append("ate finish")
+        toString()
+    }
+    println(result3)
     //吃完所有水果并打印,demo2
     println("--------------demo2---------------------")
     val list2= listOf("apple","banana","pear")
